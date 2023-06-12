e93976ea146406c016d24ff053bef6ad9a99f47e
paul <paulj.hidalgo@gmail.com>
2023-06-12 13:18:25 -0300

    updating doxygen files

--- html/html/search/functions_6.js
+++ html/html/search/functions_6.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['update_0',['Update',['../classProjetoFinal_1_1Map.html#acce63123ff60132b4b89d91e849f0013',1,'ProjetoFinal::Map']]]
+  ['update_0',['Update',['../classProjetoFinal_1_1Map.html#acce63123ff60132b4b89d91e849f0013',1,'ProjetoFinal::Map']]],
+  ['updatemap_1',['UpdateMap',['../classProjetoFinal_1_1Map.html#a87d1e3fa06cad119c4fb9093c2159562',1,'ProjetoFinal::Map']]],
+  ['updaterobot_2',['UpdateRobot',['../classProjetoFinal_1_1Robot.html#a7810ea7aa5d1a3c6dbcb7b2830f3fdf9',1,'ProjetoFinal::Robot']]]
 ];
